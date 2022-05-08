0221d2e4cee26950e030076ab41c63b78bc39b28
FastEddy1 <edwardrmock@gmail.com>
2022-05-08 00:36:35 -0600

    testing logic and event listener, WIP

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 // Assignment code here
 
-var userLength = Number(window.prompt("How many characters total do you want to include? \n Minimum of 8 and maximum of 128 characters"));
-  if ((userLength <7)) {window.alert("Error: Please enter a number between 8 and 128");}
-  else if ((userLength >129)) {window.alert("Error: Please enter a number between 8 and 128");} 
-  else (userLength > 7 === 129);
-console.log(userLength);
 
-var userNumber = (window.confirm("Should the password include numbers? \n Ok for Yes, Cancel for No"));
-console.log(userNumber);
 
-var userSymbol = (window.confirm("Should the password include symbols? \n Ok for Yes, Cancel for No"));
-console.log(userSymbol);
-
-var userUppercase = (window.confirm("Should the password include upper-case letters? \n Ok for Yes, Cancel for No"));
-console.log(userUppercase);
-
-var userLowercase = (window.confirm("Should the password include lower-case letters? \n Ok for Yes, Cancel for No"));
-console.log(userLowercase);
-
+  password = writePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -32,21 +18,43 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-function generatePassword() {
-  var passLength = (userLength,number);
-  var numberSet = '0123456789';
-  var symbolSet = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-  var upperClassSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var lowerClassSet = 'abcdefghijklmnopqrstuvwxyz';
+document.getElementById(button).setProperty('button', ['disabled = false']).addEventListener('button', 'clicked'), generatePassword(); //Couldn't get this working
+ 
 
-  
-  if (userNumber) numberSet; 
-  else if (userSymbol == true) symbolSet;
-  else if (userUppercase == true) upperClassSet;
-  else if (userLowercase == true) lowerClassSet;
+function generatePassword() 
+  {
+    var userLength = Number(window.prompt("How many characters total do you want to include? \n Minimum of 8 and maximum of 128 characters"));
+      if ((userLength <7)) {window.alert("Error: Please enter a number between 8 and 128");}
+      else if ((userLength >129)) {window.alert("Error: Please enter a number between 8 and 128");} 
+      //Need to figure out how to prevent user from continuing after error alert
+      else (userLength > 7 === 129); //not sure this is doing anything
+    console.log(userLength);
+    
+    var userNumber = (window.confirm("Should the password include numbers? \n Ok for Yes, Cancel for No"));
+    console.log(userNumber);
+    
+    var userSymbol = (window.confirm("Should the password include symbols? \n Ok for Yes, Cancel for No"));
+    console.log(userSymbol);
+    
+    var userUppercase = (window.confirm("Should the password include upper-case letters? \n Ok for Yes, Cancel for No"));
+    console.log(userUppercase);
+    
+    var userLowercase = (window.confirm("Should the password include lower-case letters? \n Ok for Yes, Cancel for No"));
+    console.log(userLowercase);
+    
+    var passLength = (userLength);
+    var numberSet = '0123456789';
+    var symbolSet = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+    var upperClassSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowerClassSet = 'abcdefghijklmnopqrstuvwxyz';
+    
+    var characterSet = [numberSet + upperClassSet + lowerClassSet + symbolSet];
+      console.log(characterSet, )
 
-  
-
-  document.getElementById("button").disabled("false")
-  generateBtn.addEventListener("click", generatePassword);
-}
+      //logic below is broken currently, still working this part out... been looking at various password generator functions and experimenting
+      for (var i = 0; i < passLength; i++) {
+        characterSet = Math.floor(Math.random() *(userLength - 1));
+        passwordText += toString((characterSet));
+      }
+    return passwordText;
+  }
