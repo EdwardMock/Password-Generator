09a21c91cf77dd971134a07f4d13fad9842c679c
FastEddy1 <edwardrmock@gmail.com>
2022-05-07 20:42:05 -0600

    Refactored variables to remove functions

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
 // Assignment code here
 //Symbols " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var userNumber = Number(window.prompt("How many characters total do you want to include? Minimum of 8 and maximum of 128 characters", "8"));
+console.log(userNumber);
 
-var userNumber = function() {
-  prompt("How many characters total do you want to include? Minimum of 8 and maximum of 128 characters");
-  if (userNumber <8) alert("Please enter a number that is 8 or larger. Maximum of 128 characters")
-  else if (userNumber > 128) alert("Please enter a number that is less than 128 characters. Minimum of 128 characters")
-};
+var userSymbol = (window.confirm("Should the password include symbols? Yes or No", "yes"));
+console.log(userSymbol);
 
-var userSymbol = function() {
-  prompt("Should the password include symbols? Yes or No");
-  if (userSymbol == "yes") userSymbol == "true";
-  else if (userSymbol == "no") userSymbol == "false";
-  else (userSymbol == ""); alert("Please enter yes or no for symbols")
-  prompt("Should the password include symbols? Yes or No")
-}
+var userUppercase = (window.confirm("Should the password include upper-case letters? Yes or No", "yes"));
+console.log(userUppercase);
 
-var userUppercase = function() {
-  prompt("Should the password include upper case letters? Yes or No");
-  if (userUppercase == "yes") userUppercase == "true";
-  else if (userUppercase == "no") userUppercase == "false";
-  else (userUppercase == ""); alert("Please enter yes or no for symbols")
-  prompt("Should the password include symbols? Yes or No")
-}
-
-var userLowercase = function() {
-  prompt("Should the password include upper case letters? Yes or No");
-  if (userLowercase == "yes") userLowercase == "true";
-  else if (userLowercase == "no") userLowercase == "false";
-  else (userLowercase == ""); alert("Please enter yes or no for symbols")
-  prompt("Should the password include symbols? Yes or No")
-}
-
+var userLowercase = (window.confirm("Should the password include lower-case letters? Yes or No"));
+console.log(userLowercase);
 
 
 // Get references to the #generate element
@@ -46,4 +26,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  document.getElementById("button").disabled("false")
+  generateBtn.addEventListener("click", generatePassword);
+}
